855d7a0f5f03795a480e79586db06b4c74333a46
listguy <nitzanjl@gmail.com>
2021-07-26 20:11:58 +0300

    added better code

--- game/Assets/Scripts/Player.cs
+++ game/Assets/Scripts/Player.cs
@@ -4,29 +4,62 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] float speed = 5f;
-    [SerializeField] Camera camera; 
+    public float speed = 5f;
 
-    // Update is called once per frame
-    void Update()
+    private float screenHalfWidthInWorldUnits;
+    private void Start()
+    {
+        float halfPlayerWidth = transform.localScale.x / 2f;
+        screenHalfWidthInWorldUnits = Camera.main.aspect * Camera.main.orthographicSize + halfPlayerWidth;
+    }
+
+    private void Update()
     {
         float inputX = Input.GetAxisRaw("Horizontal");
-        Vector2 direction = new Vector2(inputX, 0);
+        float velocity = inputX * speed;
 
-        transform.Translate(direction * speed * Time.deltaTime);
-        if (!isOnScreen())
+        transform.Translate(Vector2.right * velocity * Time.deltaTime);
+
+        if (transform.position.x > screenHalfWidthInWorldUnits)
         {
-            Vector3 playerWorldPos = camera.WorldToViewportPoint(transform.position);
-            transform.position = (camera.ViewportToWorldPoint(new Vector3((Mathf.Round(playerWorldPos.x) + 1) % 2, playerWorldPos.y, playerWorldPos.z)));
+            // if player crossed the right border, 'teleport' it to the left
+            transform.position = new Vector2(-screenHalfWidthInWorldUnits, transform.position.y);
+        }
+
+        if (transform.position.x < -screenHalfWidthInWorldUnits)
+        {
+            // if player crossed the left border, 'teleport' it to the right
+            transform.position = new Vector2(screenHalfWidthInWorldUnits, transform.position.y);
         }
     }
-
-    // Tell wether the player is seen by the camera or not
-    bool isOnScreen()
-    {
-        Vector3 playerWorldPos = camera.WorldToViewportPoint(transform.position);
-
-        return playerWorldPos.x > 0 && playerWorldPos.x < 1;
-      
-    }
 }
+
+// My code
+//public class Player : MonoBehaviour
+//{
+//    [SerializeField] float speed = 5f;
+//    [SerializeField] Camera camera; 
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        float inputX = Input.GetAxisRaw("Horizontal");
+//        Vector2 direction = new Vector2(inputX, 0);
+
+//        transform.Translate(direction * speed * Time.deltaTime);
+//        if (!isOnScreen())
+//        {
+//            Vector3 playerWorldPos = camera.WorldToViewportPoint(transform.position);
+//            transform.position = (camera.ViewportToWorldPoint(new Vector3((Mathf.Round(playerWorldPos.x) + 1) % 2, playerWorldPos.y, playerWorldPos.z)));
+//        }
+//    }
+
+//    // Tell wether the player is seen by the camera or not
+//    bool isOnScreen()
+//    {
+//        Vector3 playerWorldPos = camera.WorldToViewportPoint(transform.position);
+
+//        return playerWorldPos.x > 0 && playerWorldPos.x < 1;
+      
+//    }
+//}
