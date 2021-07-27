cf7fe2ff3723757307893c0c90d5f01900316c32
listguy <nitzanjl@gmail.com>
2021-07-27 20:10:22 +0300

    update code to falling blocks
    changed the code following video instructions

--- game/Assets/Scripts/FallingBlock.cs
+++ game/Assets/Scripts/FallingBlock.cs
@@ -4,31 +4,43 @@ using UnityEngine;
 
 public class FallingBlock : MonoBehaviour
 {
-    [SerializeField] Rigidbody2D fallingBlock;
-    private float screenHalfWidthInWorldUnits;
-    private float screenHeightInWorldUnits;
-    private float yAxisSpanPoint;
-    // Start is called before the first frame update
-    void Start()
+    
+    [SerializeField] float speed = 7f;
+    private void Update()
     {
-        float halfBlockWidth = fallingBlock.transform.localScale.x / 2f;
-        screenHalfWidthInWorldUnits = Camera.main.aspect * Camera.main.orthographicSize - halfBlockWidth;
-        screenHeightInWorldUnits = (1 / Camera.main.aspect) * 2 * (screenHalfWidthInWorldUnits + halfBlockWidth);
-        yAxisSpanPoint = screenHeightInWorldUnits / 2 + fallingBlock.transform.localScale.y;
-        InvokeRepeating("spawnBlock", 1, 1);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    private void spawnBlock()
-    {
-        Rigidbody2D instance = Instantiate(fallingBlock, new Vector3(Random.Range(-screenHalfWidthInWorldUnits, screenHalfWidthInWorldUnits), yAxisSpanPoint), Quaternion.Euler(0, 0, 0));
-
-        instance.velocity = Vector2.up * -3;
-        Destroy(instance.gameObject, 5);
+        transform.Translate(Vector2.down * speed * Time.deltaTime);
     }
 }
+
+// My code that is responsible for block spawning. Video instructions have it in another script called 'Spawner'
+// This script now handles moving the block down and destroying it
+//public class FallingBlock : MonoBehaviour
+//{
+//    [SerializeField] Rigidbody2D fallingBlock;
+//    private float screenHalfWidthInWorldUnits;
+//    private float screenHeightInWorldUnits;
+//    private float yAxisSpanPoint;
+//    // Start is called before the first frame update
+//    void Start()
+//    {
+//        float halfBlockWidth = fallingBlock.transform.localScale.x / 2f;
+//        screenHalfWidthInWorldUnits = Camera.main.aspect * Camera.main.orthographicSize - halfBlockWidth;
+//        screenHeightInWorldUnits = (1 / Camera.main.aspect) * 2 * (screenHalfWidthInWorldUnits + halfBlockWidth);
+//        yAxisSpanPoint = screenHeightInWorldUnits / 2 + fallingBlock.transform.localScale.y;
+//        InvokeRepeating("spawnBlock", 1, 1);
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+
+//    }
+
+//    private void spawnBlock()
+//    {
+//        Rigidbody2D instance = Instantiate(fallingBlock, new Vector3(Random.Range(-screenHalfWidthInWorldUnits, screenHalfWidthInWorldUnits), yAxisSpanPoint), Quaternion.Euler(0, 0, 0));
+
+//        instance.velocity = Vector2.up * -3;
+//        Destroy(instance.gameObject, 5);
+//    }
+//}
---
+++ game/Assets/Scripts/Spawner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] GameObject fallingBlockPrefab;
+    [SerializeField] float secondsBetweenSpawns = 1;
+
+    // store the sizes as a (x,y)
+    private Vector2 screenHalfSizeWorldUnits;
+    private float nextSpawnTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenHalfSizeWorldUnits = new Vector2(Camera.main.aspect * Camera.main.orthographicSize, Camera.main.orthographicSize);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > nextSpawnTime)
+        {
+            nextSpawnTime = Time.time + secondsBetweenSpawns;
+
+            float halfPlayerHeight = 0.5f;
+            Vector2 spawnPosition = new Vector2(Random.Range(-screenHalfSizeWorldUnits.x, screenHalfSizeWorldUnits.x), screenHalfSizeWorldUnits.y + halfPlayerHeight);
+
+            Instantiate(fallingBlockPrefab, spawnPosition, Quaternion.identity);
+        }
+    }
+}
+
+// Spawning blocks code moved here from FallingBlock after video instructions
